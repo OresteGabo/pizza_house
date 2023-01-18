4e27594863cf1eecbb7cbc56e0913cc1ec45700a
oreste <muhirwa.g.oreste@gmail.com>
2023-01-18 13:50:00 +0100

    Pizzas view

---
+++ resources/views/pizzas.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.layout')
+@section('content')
+    <div class="flex-center position-ref ">
+        <div class="content">
+            <div class="title m-b-md">
+                PZ
+            </div>
+        </div>
+    </div>
+@endsection
